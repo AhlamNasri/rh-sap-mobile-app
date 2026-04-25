7103d62e0f8c15bd0c64cf64103ca9b3374d838c
AhlamNasri <ahlam2004nasri@gmail.com>
2026-04-25 08:24:18 +0100

    init: Spring Boot project structure + packages

---
+++ src/main/java/com/rhsap/RhSapApplication.java
@@ -0,0 +1,13 @@
+package com.rhsap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhSapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhSapApplication.class, args);
+	}
+
+}
